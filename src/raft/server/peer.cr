9160a6eaa0d7691cb6a8f4c699a19c13edceb428
galvertez <galvertez@gmail.com>
2019-09-11 01:14:47 -0400

    simplify peer

--- src/raft/server/peer.cr
+++ src/raft/server/peer.cr
@@ -4,25 +4,36 @@ class Raft::Server::Peer
   @uri : URI
   @socket : IO::Buffered|OpenSSL::SSL::Socket::Client?
 
+  getter next_index : UInt64
+  getter match_index : UInt64
+
   def initialize(@address : String, @ssl : OpenSSL::SSL::Context? = nil)
     @uri = URI.parse(string)
     @socket = nil
   end
 
-  def socket
+  def ssl=(ssl : OpenSSL::SSL::Context)
+    @ssl = ssl
+    socket
+  end
+
+  private def socket
     return @socket if @socket && !@socket.closed?
     skt = TCPSocket.new(@uri.host, @uri.port)
-    if @ssl
-      @socket = OpenSSL::SSL::Socket::Client.new(skt, context: @ssl, sync_close: true, hostname: @uri.host)
+    ssl = @ssl
+    if ssl
+      @socket = OpenSSL::SSL::Socket::Client.new(skt, context: ssl, sync_close: true, hostname: @uri.host)
     else
       @socket = skt
     end
     @socket
   end
 
-  def request_vote(request)
+  def send(request : Raft::RPC::Packet)
     request.to_io(socket, FM)
-    Fiber.yield
-    Raft::RPC::RequestVote::Result.new(socket, FM)
+  end
+
+  def read
+    packet = Raft::RPC::Packet.new(socket, FM)
   end
 end
